3af701e6f0cc88462338196a9ff7799c4db2e56c
nhat.nguyen <hoangnhat2911@gmail.com>
2016-03-10 18:33:57 -0500

    added basic routes

--- server.js
+++ server.js
@@ -1,13 +1,71 @@
 var PORT = 3000 || process.env.PORT;
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var dota = require('./dota2.js');
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
 app.get('/rankings', function(req, res) {
-    
+    dota.getTeamsRankings().then(function(data) {
+        res.json(data);
+    }, function(err) {
+        req.status(500).send();
+    });
+});
+
+app.get('/logos', function(req, res) {
+    dota.getTeamsLogos().then(function(data) {
+        res.json(data);
+    }, function(err) {
+        req.status(500).send();
+    });
+});
+
+app.get('/teams/:id', function(req, res) {
+    var id = req.params.id;
+    dota.getTeamData(id).then(function(data) {
+        res.json(data);
+    }, function(err) {
+        req.status(500).send();
+    });
+});
+
+app.get('/matches/live', function(req, res) {
+    dota.getLiveMatches().then(function(data) {
+        res.json(data);
+    }, function(err) {
+        req.status(500).send();
+    });
+});
+
+app.get('/matches/recent', function(req, res) {
+    dota.getRecentMatchesResults().then(function(data) {
+        res.json(data);
+    }, function(err) {
+        req.status(500).send();
+    });
+});
+
+app.get('/matches/upcoming', function(req, res) {
+    dota.getUpcomingMatches().then(function(data) {
+        res.json(data);
+    }, function(err) {
+        req.status(500).send();
+    });
+});
+
+app.get('/heroes/:name', function(req, res) {
+    var name = req.params.name;
+    dota.getHeroStats(name).then(function(data) {
+        res.json(data);
+    }, function(err) {
+        req.status(500).send();
+    });
 });
 
 app.listen(PORT, function() {
